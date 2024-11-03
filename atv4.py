47c9272a9bf7b9a9d8295819e638780854bb6d61
JulioX1047 <julioxmenezes@gmail.com>
2024-11-03 16:28:10 -0300

    Add files via upload

---
+++ atv4.py
@@ -0,0 +1,10 @@
+palestrantes = (
+    ("Ana Silva", "Inovação em Tecnologia", "Universidade A"),
+    ("Carlos Souza", "Sustentabilidade e Meio Ambiente", "Instituto B"),
+    ("Mariana Lima", "Educação e Tecnologia", "Faculdade C")
+)
+
+terceiro_palestrante = palestrantes[2]
+print("Nome:", terceiro_palestrante[0])
+print("Tema da Palestra:", terceiro_palestrante[1])
+print("Instituição:", terceiro_palestrante[2])
